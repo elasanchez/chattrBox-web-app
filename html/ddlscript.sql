b427501e543f7c07906be595f92bea07c2f95a49
Luigi Sanchez <elasanchez@users.noreply.github.com>
2018-04-16 23:12:39 -0700

    Delete ddlscript.sql

--- html/ddlscript.sql
+++
@@ -1,8 +0,0 @@
-DROP DATABASE IF EXISTS university;
-CREATE DATABASE university;
-
-USE university;
-
-CREATE TABLE student(cwid INT NOT NULL PRIMARY KEY, fname VARCHAR(15) NOT NULL, 
-	lname VARCHAR(15) NOT NULL, address VARCHAR(20), city VARCHAR(15), zip INT );
-
